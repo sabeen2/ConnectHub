fd836ddb1e425f4b9b847c98187d659d5b1beeb2
Sabin Pandey <sabeenpandey2@gmail.com>
2024-07-18 10:33:26 +0545

    Initial setup of project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="mx-auto">
+      <h1 className="text-3xl font-bold text-indigo-500">Hello Discord</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
