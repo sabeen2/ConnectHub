7ccacd5aa7e3d21d425364189815e9355cc04595
Sabin Pandey <sabeenpandey2@gmail.com>
2024-07-18 12:23:59 +0545

    Initial Auth routes setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="mx-auto">
-      <h1 className="text-3xl font-bold text-indigo-500">Hello Discord</h1>
+      <h1 className="text-3xl font-bold text-indigo-500">
+        This is protected routes
+      </h1>
       <Button>Button</Button>
     </div>
   );
